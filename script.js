a3facb2f314ea2f1972aef71aca37f4c3f19fa5e
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-09-09 14:43:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const bici = [
+    { nome: "Colnago", peso: 35},
+    { nome: "Bergamont", peso: 48},
+    { nome: "Factor", peso: 45},
+    { nome: "Trek", peso: 41},
+    { nome: "Amflow", peso: 49},
+    { nome: "Scott", peso: 38}
+];
+
+let pesoPiuBasso;
+
+
+for (let i = 0; i < bici.length; i++) {
+    const biciComplete = bici[i];
+    //console.log(pesoBici.peso);
+    pesiBici = biciComplete.peso
+    console.log(pesiBici);
+    
+}
+
+
+
+
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+// squadre
+const squadre = [
+    { nome: "Liverpool", puntiFatti: 0, falliSubiti: 0},
+    { nome: "Barcellona", puntiFatti: 0, falliSubiti: 0},
+    { nome: "Liverpool", puntiFatti: 0, falliSubiti: 0},
+    { nome: "Real Madrid", puntiFatti: 0, falliSubiti: 0}
+];
+
+// creazione numeri casuali
+function puntiFattiCasuale(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function inserimentoGol(punteggi, min, max) {
+  // Itera sulle chiavi dell'oggetto
+  for (const key in punteggi) {
+    // Controlla che la proprietà appartenga all'oggetto stesso
+    if (Object.hasOwnProperty.call(obj, key)) {
+      // Genera un nuovo valore casuale per la proprietà
+      obj[key] = generaNumeroCasuale(min, max);
+    }
+  }
+  return obj;
+}
